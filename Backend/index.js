eb8c9076e48602e2280daaeaab7774bbbad37371
rohitchauhan45 <chauhanrohith.3277@gmail.com>
2026-02-04 14:43:18 +0530

    Update allowed origins in index.js and mark subproject as dirty

--- Backend/index.js
+++ Backend/index.js
@@ -21,8 +21,7 @@ const allowedOrigins = [
 	'https://fe2-wp.vercel.app',
 	'http://localhost:5173',
 	'http://localhost:3000',
-	'http://ded3896.inmotionhosting.com',
-	'https://ded3896.inmotionhosting.com',
+	'https://frontend.coredocassist.com'
 ];
 
 // Add frontendUrl from env if it's not already in the list
